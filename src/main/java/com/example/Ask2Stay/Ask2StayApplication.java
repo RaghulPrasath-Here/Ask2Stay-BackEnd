d7118c69cee4c19e3536991fb286cb18fb18067e
RaghulPrasath-Here <raghulprasathhere@gmail.com>
2025-08-03 20:24:41 +0100

    first commit

---
+++ src/main/java/com/example/Ask2Stay/Ask2StayApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ask2Stay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ask2StayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ask2StayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ask2Stay/Ask2StayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ask2Stay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ask2StayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
